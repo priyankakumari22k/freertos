2a56a4cc081cf3954c578647d12812080555d52d
kriya22k <priyanka.kuamri@einfochips.com>
2023-06-01 12:12:13 +0530

    Adding freeRTOS thread creating project of STM32L5

---
+++ STM32CubeIDE/Debug/Middlewares/FreeRTOS/port.d
@@ -0,0 +1,35 @@
+Middlewares/FreeRTOS/port.o: \
+ C:/Users/158180/STM32CubeIDE/workspace_1.7.0/STM32CubeL5/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM33_NTZ/non_secure/port.c \
+ ../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h \
+ ../../Inc/FreeRTOSConfig.h \
+ ../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ ../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ ../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ ../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM33_NTZ/non_secure/portmacro.h \
+ ../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
+ ../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/task.h \
+ ../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/list.h \
+ ../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
+ C:/Users/158180/STM32CubeIDE/workspace_1.7.0/STM32CubeL5/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM33_NTZ/non_secure/portasm.h
+
+../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
+
+../../Inc/FreeRTOSConfig.h:
+
+../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+
+../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+
+../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+
+../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM33_NTZ/non_secure/portmacro.h:
+
+../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
+
+../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/task.h:
+
+../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/list.h:
+
+../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
+
+C:/Users/158180/STM32CubeIDE/workspace_1.7.0/STM32CubeL5/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM33_NTZ/non_secure/portasm.h:
---
+++ STM32CubeIDE/Debug/Middlewares/FreeRTOS/portasm.d
@@ -0,0 +1,29 @@
+Middlewares/FreeRTOS/portasm.o: \
+ C:/Users/158180/STM32CubeIDE/workspace_1.7.0/STM32CubeL5/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM33_NTZ/non_secure/portasm.c \
+ C:/Users/158180/STM32CubeIDE/workspace_1.7.0/STM32CubeL5/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM33_NTZ/non_secure/portasm.h \
+ ../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h \
+ ../../Inc/FreeRTOSConfig.h \
+ ../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h \
+ ../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h \
+ ../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h \
+ ../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM33_NTZ/non_secure/portmacro.h \
+ ../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h \
+ ../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h
+
+C:/Users/158180/STM32CubeIDE/workspace_1.7.0/STM32CubeL5/Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM33_NTZ/non_secure/portasm.h:
+
+../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/FreeRTOS.h:
+
+../../Inc/FreeRTOSConfig.h:
+
+../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/projdefs.h:
+
+../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/portable.h:
+
+../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/deprecated_definitions.h:
+
+../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/portable/GCC/ARM_CM33_NTZ/non_secure/portmacro.h:
+
+../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
+
+../../../../../../../Middlewares/Third_Party/FreeRTOS/Source/include/mpu_wrappers.h:
